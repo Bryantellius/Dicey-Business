bfe5e950d37227b852a77c4777e89404c4dfe5f6
Ben Bryant <brbryant2639@gmail.com>
2020-02-22 09:23:45 -0600

    finished objectives, click listeners for single roll, remove

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ let diceContainer = document.getElementById('diceContainer');
 let generatorBtn = document.getElementById('generatorBtn');
 let rollBtn = document.getElementById('rollBtn');
 let sumBtn = document.getElementById('sumBtn');
+let feedback = document.getElementById('feedback');
 let newDie;
 let dieArr = [];
 
@@ -10,12 +11,21 @@ generatorBtn.addEventListener('click', () => {
     console.log(die);
 })
 
-rollBtn.addEventListener('click', ()=>{
+rollBtn.addEventListener('click', () => {
     console.log(dieArr);
-    dieArr.forEach((object)=>{
+    dieArr.forEach((object) => {
         object.value = Math.round(Math.random() * (6 - 1) + 1);
         object.div.textContent = object.value;
     })
+})
+
+sumBtn.addEventListener('click', () => {
+    const sum = (acc, object) => acc + object.value;
+    if(dieArr.length===1){
+        feedback.textContent = "The die's value is " + dieArr[0].value;
+    }else{
+        feedback.textContent = 'The sum of the dice = ' + dieArr.reduce(sum, 0);
+    }
 })
 
 class Die {
@@ -31,12 +41,26 @@ class Die {
         newDie = document.createElement('div');
         newDie.className = 'die';
         newDie.textContent = this.value;
+        newDie.addEventListener('click', (event) => {
+            this.roll();
+            this.div.textContent = this.value;
+            event.preventDefault();
+        })
+        newDie.addEventListener('dblclick', () => {
+            this.div.remove();
+            dieArr.forEach((object) => {
+                if (object === this) {
+                    dieArr = dieArr.filter((object) => !(object === this));
+                }
+            })
+
+        })
         this.div = newDie;
         diceContainer.appendChild(newDie);
     }
+
     roll = () => {
         this.value = Math.round(Math.random() * (6 - 1) + 1);
-
     }
 
 }
